3f063be8178d9d9072be340271b618dfd7123090
clubby789 <jamie@hill-daniel.co.uk>
2021-02-25 20:36:30 +0000

    Fix event loop error on Python 3.8/Windows

--- hackthebox/htb.py
+++ hackthebox/htb.py
@@ -3,11 +3,31 @@ from typing import List, Callable
 import base64
 import json
 import time
+import os
+
 import requests
 import aiohttp
 
 from .constants import API_BASE, USER_AGENT
 from .errors import UnknownSolveException, AuthenticationException
+
+
+if os.name == 'nt':
+    # https://github.com/aio-libs/aiohttp/issues/4324#issuecomment-733884349
+    from functools import wraps
+    # noinspection PyProtectedMember
+    from asyncio.proactor_events import _ProactorBasePipeTransport
+
+    def silence_event_loop_closed(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except RuntimeError as e:
+                if str(e) != 'Event loop is closed':
+                    raise
+        return wrapper
+    _ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
 
 
 def check_expired_jwt(token: str) -> bool:
