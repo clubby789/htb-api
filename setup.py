8b61c4f33dfe860820b693c783c82ac00238bbca
clubby789 <jamie@hill-daniel.co.uk>
2021-02-26 13:29:35 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="htb-api-clubby789",
+    name="hackthebox-py-clubby789",
     version="0.0.1",
     author="clubby789@github.com",
-    author_email="clubby789@gmaol.com",
-    description="A wrapper for the HTB API.",
+    author_email="clubby789@gmail.com",
+    description="A wrapper for the Hack The Box API.",
     long_description=long_description,
     classifiers=[
         "Programming Language :: Python :: 3",
